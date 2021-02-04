c23b391af3881f5d97301a887766865a394b9ad0
rafenasir <rafenasir@gmail.com>
2021-02-04 23:39:25 +0100

    seperted the Handler Function

---
+++ handler.js
@@ -0,0 +1,56 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+async function postNews(req, resp) {
+    // get this from database
+    const data = req.payload;
+    console.log({ data });
+
+    // save post body into db and return it
+    try {
+        const news = await prisma.news.create({ data })
+        return resp.response(news)
+
+    } catch (e) {
+        console.log(e);
+        throw new Error(e)
+    }
+}
+
+async function putNews(req, resp) {
+    try {
+        const news = await prisma.news.update({
+            where: {
+                id: req.payload.id
+            },
+            data: {
+                title: req.payload.title,
+                body: req.payload.body,
+            }
+        });
+        return resp.response(news)
+    } catch (e) {
+        console.log(e);
+        throw new Error(e)
+    }
+}
+
+async function deleteNews(req, resp) {
+    try {
+        const news = await prisma.news.delete({
+            where: {
+                id: req.payload.id
+            }
+        });
+        return resp.response(news)
+
+    } catch (e) {
+        console.log(e);
+        throw new Error(e)
+    }
+}
+
+module.exports = {
+    postNews,
+    putNews,
+    deleteNews
+}
--- routes.js
+++ routes.js
@@ -1,139 +1,142 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const Joi = require("joi");
+const handler = require("./handler")
 module.exports = [{
-        method: "GET",
-        path: "/api/news",
-        options: {
-            handler: async(request, resp) => {
-                try {
-                    const news = await prisma.news.findMany();
-                    return resp.response(news);
-                } catch (e) {
-                    console.log(e);
-                    throw new Error(e);
+            method: "GET",
+            path: "/api/news",
+            options: {
+                handler: async(req, resp) => {
+                    try {
+                        const news = await prisma.news.findMany();
+                        return resp.response(news);
+                    } catch (e) {
+                        console.log(e);
+                        throw new Error(e);
+                    }
+                },
+                tags: ["api"],
+            },
+
+        },
+
+        {
+            method: 'POST',
+            path: '/api/news',
+            options: {
+                handler: handler.postNews,
+                tags: ['api'],
+
+                validate: {
+                    payload: Joi.object({
+                        title: Joi.string().required(),
+                        body: Joi.string().required(),
+                    }).label("postNews"),
+                },
+            },
+        },
+
+        {
+            method: 'PUT',
+            path: '/api/news',
+            options: {
+                handler: handler.putNews,
+                tags: ['api'],
+                validate: {
+                    payload: Joi.object({
+                        id: Joi.string().required(),
+                        title: Joi.string().required(),
+                        body: Joi.string().required(),
+                    }).label("putNews"),
+                },
+            },
+        },
+
+        {
+            method: 'DELETE',
+            path: '/api/news',
+            options: {
+                handler: handler.deleteNews,
+                tags: ['api'],
+
+                validate: {
+                    payload: Joi.object({
+                        id: Joi.string().required(),
+                    }).label("deleteNews"),
                 }
             },
-            tags: ["api"],
         },
-    },
-    {
-        method: 'GET',
-        path: '/api/about_us',
-        options: {
-            handler: async(req, resp) => {
-                try {
-                    const aboutUs = await prisma.about.findMany();
-                    return resp.response(aboutUs);
-                } catch (e) {
-                    console.log(e);
-                    throw new Error(e);
-                }
-                // get this from database
-            },
 
-            tags: ['api'],
-        }
-    },
-    {
-        method: 'GET',
-        path: '/api/contact_us',
-        options: {
-            handler: (request, response) => {
-                // get this from database
-                return { title: "contact us", body: "this is contact us page" };
-            },
-            tags: ['api'],
+    ]
+    // {
+    //     method: 'GET',
+    //     path: '/api/about_us',
+    //     options: {
+    //         handler: async(req, resp) => {
+    //             try {
+    //                 const aboutUs = await prisma.about.findMany();
+    //                 return resp.response(aboutUs);
+    //             } catch (e) {
+    //                 console.log(e);
+    //                 throw new Error(e);
+    //             }
+    //             // get this from database
+    //         },
 
-        },
-    },
-    {
-        method: 'POST',
-        path: '/api/news',
-        options: {
-            handler: async(req, resp) => {
-                // get this from database
-                const data = req.payload
-                console.log({ data });
-
-                // save post body into db and return it
-                try {
-                    const news = await prisma.news.create({ data })
-                    return news;
-
-                } catch (e) {
-                    console.log(e);
-                    throw new Error(e)
-                }
-            },
-            tags: ["api"],
-            validate: {
-                payload: Joi.object({
-                    title: Joi.string().required(),
-                    body: Joi.string().required(),
-                }).label("disconnectUserLocation"),
-            },
-
-        },
-    },
-    {
-        method: 'POST',
-        path: '/api/contact_us',
-        options: {
-            handler: (request, response) => {
-                const postresponse = request.body
-                console.log({ postresponse });
-                return postresponse;
-            },
-            tags: ['api'],
-        }
-
-    },
-
-    {
-        method: 'POST',
-        path: '/api/about_us',
-        options: {
-            handler: (request, response) => {
-                const postresponse = request.body
-                console.log(postresponse);
-                return postresponse;
-            },
-            tags: ['api'],
-
-        }
-    },
-
-    {
-        method: 'PUT',
-        path: '/api/news',
-        options: {
-            handler: (reequest, h) => {
-                //get this from data base.
-                const postbody = request.body;
-
-                //save this post body into db and return it
-                return postbody
-            },
-            tags: ['api'],
-
-        }
+//         tags: ['api'],
+//     }
+// },
 
 
-    },
+//         tags: ["api"],
 
-    {
-        method: 'DELETE',
-        path: '/api/news',
-        options: {
-            handler: (reequest, h) => {
-                //get this from data base.
-                const postbody = request.body;
+//         validate: {
+//             payload: Joi.object({
+//                 title: Joi.string().required(),
+//                 body: Joi.string().required(),
+//             }).label("disconnectUserLocation"),
+//         },
 
-                //save this post body into db and return it
-                return postbody
-            },
-            tags: ['api'],
-        },
-    },
-]
+// {
+//     method: 'GET',
+//     path: '/api/contact_us',
+//     options: {
+//         handler: (request, response) => {
+//             // get this from database
+//             return { title: "contact us", body: "this is contact us page" };
+//         },
+//         tags: ['api'],
+
+//     },
+// },
+
+
+// //    },
+// //},
+// {
+//     method: 'POST',
+//     path: '/api/contact_us',
+//     options: {
+//         handler: (request, response) => {
+//             const postresponse = request.body
+//             console.log({ postresponse });
+//             return postresponse;
+//         },
+//         tags: ['api'],
+//     }
+
+// },
+
+// {
+//     method: 'POST',
+//     path: '/api/about_us',
+//     options: {
+//         handler: (request, response) => {
+//             const postresponse = request.body
+//             console.log(postresponse);
+//             return postresponse;
+//         },
+//         tags: ['api'],
+
+//     }
+// },
