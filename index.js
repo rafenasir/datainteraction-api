5fb3c8f87cf249b598a044d2deee65b61385b664
rafenasir <rafenasir@gmail.com>
2021-02-07 13:23:46 +0100

    Allowed CORS

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const Pack = require('./package');
 const init = async() => {
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: "localhost",
+        routes: { cors: { origin: ['*'] } }
     });
 
     const swaggerOptions = {
