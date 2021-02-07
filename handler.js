ca6fd5d97230973e4707113e22e866a625d5e7ad
rafenasir <rafenasir@gmail.com>
2021-02-07 13:23:02 +0100

    allowed the CORS in hapi server

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
+
 async function postNews(req, resp) {
     // get this from database
     const data = req.payload;
