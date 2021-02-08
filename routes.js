48151f8e85718933dbdaa52ea9a7f51389efd092
rafenasir <rafenasir@gmail.com>
2021-02-08 14:34:23 +0100

    defined the GET methode for about us page

--- routes.js
+++ routes.js
@@ -3,85 +3,87 @@ const prisma = new PrismaClient();
 const Joi = require("joi");
 const handler = require("./handler")
 module.exports = [{
-            method: "GET",
-            path: "/api/news",
-            options: {
-                handler: async(req, resp) => {
-                    try {
-                        const news = await prisma.news.findMany();
-                        return resp.response(news);
-                    } catch (e) {
-                        console.log(e);
-                        throw new Error(e);
-                    }
-                },
-                tags: ["api"],
-            },
-
-        },
-
-        {
-            method: 'POST',
-            path: '/api/news',
-            options: {
-                handler: handler.postNews,
-                tags: ['api'],
-
-                validate: {
-                    payload: Joi.object({
-                        title: Joi.string().required(),
-                        body: Joi.string().required(),
-                    }).label("postNews"),
-                },
-            },
-        },
-
-        {
-            method: 'PUT',
-            path: '/api/news',
-            options: {
-                handler: handler.putNews,
-                tags: ['api'],
-                validate: {
-                    payload: Joi.object({
-                        id: Joi.string().required(),
-                        title: Joi.string().required(),
-                        body: Joi.string().required(),
-                    }).label("putNews"),
-                },
-            },
-        },
-
-        {
-            method: 'DELETE',
-            path: '/api/news',
-            options: {
-                handler: handler.deleteNews,
-                tags: ['api'],
-
-                validate: {
-                    payload: Joi.object({
-                        id: Joi.string().required(),
-                    }).label("deleteNews"),
+        method: "GET",
+        path: "/api/news",
+        options: {
+            handler: async(req, resp) => {
+                try {
+                    const news = await prisma.news.findMany();
+                    return resp.response(news);
+                } catch (e) {
+                    console.log(e);
+                    throw new Error(e);
                 }
             },
+            tags: ["api"],
         },
 
-    ]
-    // {
-    //     method: 'GET',
-    //     path: '/api/about_us',
-    //     options: {
-    //         handler: async(req, resp) => {
-    //             try {
-    //                 const aboutUs = await prisma.about.findMany();
-    //                 return resp.response(aboutUs);
-    //             } catch (e) {
-    //                 console.log(e);
-    //                 throw new Error(e);
-    //             }
-    //             // get this from database
-    //         },
+    },
+
+    {
+        method: 'POST',
+        path: '/api/news',
+        options: {
+            handler: handler.postNews,
+            tags: ['api'],
+
+            validate: {
+                payload: Joi.object({
+                    title: Joi.string().required(),
+                    body: Joi.string().required(),
+                }).label("postNews"),
+            },
+        },
+    },
+
+    {
+        method: 'PUT',
+        path: '/api/news',
+        options: {
+            handler: handler.putNews,
+            tags: ['api'],
+            validate: {
+                payload: Joi.object({
+                    id: Joi.string().required(),
+                    title: Joi.string().required(),
+                    body: Joi.string().required(),
+                }).label("putNews"),
+            },
+        },
+    },
+
+    {
+        method: 'DELETE',
+        path: '/api/news',
+        options: {
+            handler: handler.deleteNews,
+            tags: ['api'],
+
+            validate: {
+                payload: Joi.object({
+                    id: Joi.string().required(),
+                }).label("deleteNews"),
+            }
+        },
+    },
+
+    {
+        method: 'GET',
+        path: '/api/about_us',
+        options: {
+            handler: async(req, resp) => {
+                try {
+                    const aboutUs = await prisma.pages.findMany();
+                    return resp.response(aboutUs);
+                } catch (e) {
+                    console.log(e);
+                    throw new Error(e);
+                }
+            },
+            tags: ["api"],
+        }
+    },
+]
 
 //         tags: ['api'],
 //     }
